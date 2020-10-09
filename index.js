f7535b0b98a12cb106bfc0611b7a1e60be06ebd2
Nicolas LE NEZET <nicolaslenezet@hotmail.fr>
2020-10-09 23:59:16 +0200

    Base app

---
+++ index.js
@@ -0,0 +1,67 @@
+import 'ol/ol.css';
+import Draw from 'ol/interaction/Draw';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import { OSM, Vector as VectorSource } from 'ol/source';
+import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer';
+import GeoJSON from 'ol/format/GeoJSON';
+import Feature from 'ol/Feature';
+var raster = new TileLayer({
+    source: new OSM(),
+});
+
+var drawings = [];
+
+var source = new VectorSource({ wrapX: false });
+
+var vector = new VectorLayer({
+    source: source,
+    renderBuffer: 100000
+});
+
+var map = new Map({
+    layers: [raster, vector],
+    target: 'map',
+    view: new View({
+        center: [-11000000, 4600000],
+        zoom: 4,
+    }),
+});
+
+var typeSelect = document.getElementById('type');
+
+var draw; // global so we can remove it later
+function addInteraction() {
+    var value = typeSelect.value;
+
+    if (value !== 'None') {
+        draw = new Draw({
+            source: source,
+            type: typeSelect.value,
+        });
+        map.addInteraction(draw);
+        draw.on('drawend', function (evt) {
+            console.log('drawend');
+            var format = new GeoJSON();
+            var geom = evt.feature.getGeometry();
+            geom.transform('EPSG:3857', 'EPSG:4326');
+            var feature = new Feature({
+                geometry: geom
+            });
+            var obj = format.writeFeatures([feature]);
+            drawings.push(JSON.parse(obj));
+            console.log(drawings);
+        });
+    }
+}
+
+/**
+ * Handle change event.
+ */
+typeSelect.onchange = function () {
+    map.removeInteraction(draw);
+    addInteraction();
+};
+
+addInteraction();
+
